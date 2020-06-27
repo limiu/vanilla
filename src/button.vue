89574bf92ea29fd83629f3671a3dd6ebec02b3b7
limiu <845217637@qq.com>
2020-06-27 23:26:42 +0800

    添加icon 和 icon position 属性

--- src/button.vue
+++ src/button.vue
@@ -1,19 +1,25 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" >
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props:['icon','iconPosition']
+    }
 </script>
 
 <style lang="scss">
     .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+        font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);border: 1px solid var(--border-color);background: var(--button-bg);
+        display: inline-flex; justify-content: center; align-items: center;
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
         }
@@ -23,6 +29,12 @@
         }
         &:focus {
             outline: none;
+        }
+        > .icon {order: 1;margin-right: .2em}
+        > .content {order: 2}
+        &.icon-right{
+            > .icon{order:2; margin-left: .2em; margin-right: 0}
+            >.content{order: 1;}
         }
     }
     </style>

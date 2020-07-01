26981ed12e8354f62d8c6404aa6bb1024cb77110
limiu <845217637@qq.com>
2020-07-01 17:06:58 +0800

    row 和 col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'VanlaCol',
+        props:{
+            span: {
+             type: [Number,String]
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .col{
+        height: 100px;
+        background: gray;
+        width: 50%;
+        border: 1px solid red;
+
+    $class: col-;
+    @for $n from 1 through 24 {
+        &.#{$class}#{$n}{
+        width:($n/24) *100%;
+        }
+     }
+    }
+</style>
